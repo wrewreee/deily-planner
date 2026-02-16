98a2a27ec1af59f9353a66721804f38bb3cff3fc
wrewreee <csgo.fortnite3@gmail.com>
2026-02-16 15:39:14 +0300

    Innitial commit daily-planner Spring boot project

---
+++ src/test/java/com/example/dailyplanner/DailyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
